977ae5d019693e89cac1d8985325f69b59a6645f
ahmadfaikh <200411100117@student.trunojoyo.ac.id>
2022-03-13 08:44:05 +0700

    mengupload fugsi.php

---
+++ fungsi.php
@@ -0,0 +1,10 @@
+<?php
+function triangle($alas, $tinggi)
+{
+    $luas = $alas / 2 * $tinggi;
+    echo ("Luas : $luas.<br>");
+}
+
+triangle(4, 3);
+triangle(20, 9);
+triangle(40, 10);
---
+++ variable.php
@@ -0,0 +1,9 @@
+<?php
+$a = 20;
+$b = 10;
+$c = 15;
+
+echo ($a + $b . "<br>");
+echo ($c - $a . "<br>");
+echo ($b + $a * $c . "<br>");
+echo ($b * ($a - $c));
